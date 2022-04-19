7fd4e1cc992c49273ac41d99a00a99e04577b852
groot <groot@groots-MacBook-Pro.local>
2022-04-18 23:10:22 -0400

    make it better

---
+++ main.py
@@ -0,0 +1,68 @@
+import discord, re, retrieve, json, os
+from discord.ext import tasks
+from uptime import keep_alive
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+    @tasks.loop(minutes=8)
+    async def noti_check():
+        with open('noti.json') as file:
+            data = json.load(file)
+            for channels in data:
+                for courses in data[channels]:
+                    arr = courses.split(' ')
+                    if retrieve.validSection(arr[0], arr[1]) == [True, "has open seats"]:
+                        channel = client.get_channel(int(channels))
+                        for users in data[channels][courses]:
+                            await channel.send(users + " Open seat/s in " + courses)
+                        data[channels][courses].clear()
+            with open("noti.json", "w") as outfile:
+                json.dump(data, outfile, indent=4)
+    noti_check.start()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    msg = message.content
+    channel = message.channel
+    channel_id = str(channel.id)
+    course_re = (re.search(r'^!course ([\w]+[\d]+)$', msg))
+    notify_re = (re.search(r'^!notify ([\w\d]+) ([\w\d]+)$', msg))
+    if course_re:
+        for sections in retrieve.sections(course_re.group(1).lower()):
+            await channel.send(sections)
+    elif notify_re:
+        output = retrieve.validSection(notify_re.group(1).lower(), notify_re.group(2).lower())
+        if output == [True, "no seats"]:
+            course_name = notify_re.group(1) + " " + notify_re.group(2)
+            mentionID = '<@' + str(message.author.id) + '>'
+            with open('noti.json') as file:
+                data = json.load(file)
+                if channel_id not in data:
+                    data[channel_id] = {}
+                if course_name not in data[channel_id]:
+                    data[channel_id][course_name] = []
+                if mentionID not in data[channel_id][course_name]:
+                    data[channel_id][course_name].append(mentionID)
+                with open("noti.json", "w") as outfile:
+                    json.dump(data, outfile, indent=4)
+            await channel.send(mentionID + " successful")
+        else:
+            await channel.send(output[1])
+    elif msg == "!check notify":
+      out = ""
+      f = open ('noti.json', "r")
+      data = json.loads(f.read())
+      for courses in data[channel_id]:
+        for user in data[channel_id][courses]:
+          if user[2:20] == str(message.author.id):
+            out += "Notify for " + courses + '\n'
+      await channel.send(out) if len(out) > 0 else await channel.send("None")
+      
+keep_alive()
+client.run(os.environ['TOKEN'])
---
+++ retrieve.py
@@ -0,0 +1,61 @@
+import urllib.request, json
+from bs4 import BeautifulSoup
+import requests
+
+def exists(data, target):
+    if target not in data or len(data[target]) == 0:
+        return False
+    return True
+def validSection(course_name, sec):
+    try:
+    #change termId to year+08 for fall, year+12 for winter, year+01 for spring, year+05 for summer
+        url = "https://app.testudo.umd.edu/soc/search?courseId=" + course_name + "&sectionId=" + sec + "&termId=202208&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on"
+        html_text = requests.get(url).text
+        soup = BeautifulSoup(html_text, 'html.parser')
+        open_seats = int(soup.find_all('span', class_='open-seats-count')[0].text)
+        if open_seats > 0:
+          return [True, "has open seats"]
+        return [True, "no seats"]
+    except:
+        return [False, "Invalid"]
+# def validSection(course_name, course_section):
+#     try:
+#         with urllib.request.urlopen("https://api.umd.io/v1/courses/" + course_name + "/sections/" + course_section) as url:
+#             data = json.loads(url.read().decode())
+#             if int(data[0]['open_seats']) > 0:
+#                 return [True, "Has open seats"]
+#             return [True, "no seats"]
+#     except:
+#         return [False, "invalid"]
+
+def sections(course):
+    out = []
+    try:
+        with urllib.request.urlopen("https://api.umd.io/v1/courses/" + course + "/sections") as url:
+            data = json.loads(url.read().decode())
+            for i in range(len(data)):
+                section_id = '***' + data[i]['section_id'] + '*** with '
+                prof = 'TBA\n' if not exists(data[i], 'instructors') else data[i]['instructors'][0] + '\n'
+                total_seats = "" if not exists(data[i], 'seats') else 'total seats:' + data[i]['seats']
+                open_seats = "" if not exists(data[i], 'open_seats') else 'open seats: ' + data[i]['open_seats']
+                wait_list = "" if not exists(data[i], 'waitlist') else 'waitlist: ' + data[i]['waitlist']
+                l_time = " " if not exists(data[i]['meetings'][0], 'start_time') else (", " + data[i]['meetings'][0]['start_time']
+                    + '-' + data[i]['meetings'][0]['end_time'])
+                l_days = " " if not exists(data[i]['meetings'][0], 'days') else ", " + data[i]['meetings'][0]['days']
+                d_building, d_time, d_days = "", "", ""
+                if data[i]['meetings'][0]['room'] == "ONLINE":
+                    l_building = 'ONLINE '
+                else:
+                    l_building = "" if not exists(data[i]['meetings'][0], 'building') else data[i]['meetings'][0]['building']
+                if len(data[i]['meetings']) > 1 and data[i]['meetings'][1]['room'] == "ONLINE":
+                    d_building = 'Discussion: ONLINE'
+                elif len(data[i]['meetings']) > 1:
+                    d_building = "\nDiscussion: " if not exists(data[i]['meetings'][1], 'building') else "\nDiscussion: " + data[i]['meetings'][1]['building']
+                    d_time = " " if not exists(data[i]['meetings'][1], 'start_time') else (", " + data[i]['meetings'][1]['start_time']
+                        + '-' + data[i]['meetings'][1]['end_time'])
+                    d_days = " " if not exists(data[i]['meetings'][1], 'days') else ", " + data[i]['meetings'][1]['days']
+                info = "Lecture: " + l_building + l_days + l_time + d_building + d_days + d_time
+                out.append(section_id + prof + info + '\n' + total_seats + ', ' + open_seats + ', ' + wait_list + '\n\n')
+    except:
+        return ["invalid Course"]
+    return out
